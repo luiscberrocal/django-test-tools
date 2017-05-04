2854a3cc148b2c456b29034a300cb924f431915a
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2017-05-04 18:25:28 -0500

    Fixed bug of char field with letters

--- django_test_tools/management/commands/generate_factories.py
+++ django_test_tools/management/commands/generate_factories.py
@@ -27,7 +27,7 @@ class {0}Factory(DjangoModelFactory):
 
 PRINT_CHARFIELD ="""    {} = LazyAttribute(lambda x: faker.text(max_nb_chars={}))"""
 PRINT_CHARFIELD_NUM ="""    {} = LazyAttribute(lambda x: FuzzyText(length={}, chars=string.digits).fuzz())"""
-PRINT_CHARFIELD_LETTERS ="""    {} = LazyAttribute(lambda x: FuzzyText(length={}, chars=string.letters).fuzz())"""
+PRINT_CHARFIELD_LETTERS ="""    {} = LazyAttribute(lambda x: FuzzyText(length={}, chars=string.ascii_letters).fuzz())"""
 PRINT_CHARFIELD_CHOICES ="""    {} = Iterator({}.{}, getter=lambda x: x[0])"""
 PRINT_DATETIMEFIELD ="""    {} = LazyAttribute(lambda x: faker.date_time_between(start_date="-1y", end_date="now",
                                                            tzinfo=timezone(settings.TIME_ZONE)))"""
