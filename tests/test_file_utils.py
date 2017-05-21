43d0ac87d1ac4a0967a3e76f2d97b9a3ae8cafbd
Luis Carlos Berrocal <luis.berrocal.1942@gmail.com>
2017-05-21 07:59:31 -0500

    Added temporary file decorators for tests

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,10 +1,18 @@
-from django.test import TestCase
+import os
 
-from django_test_tools.file_utils import hash_file
+from django.conf import settings
+from django.test import TestCase
+from django.test import tag
+
+from django_test_tools.file_utils import hash_file, temporary_file
 from django_test_tools.mixins import TestOutputMixin
 from django_test_tools.utils import create_output_filename_with_date
 
 
+
+
+
+@tag('UNIT')
 class TestHashFile(TestOutputMixin, TestCase):
 
     def test_hash(self):
@@ -24,3 +32,25 @@ class TestHashFile(TestOutputMixin, TestCase):
         except ValueError as e:
             self.assertEqual('kkkk is not a valid hashing algorithm', str(e))
 
+    @temporary_file('json')
+    def test_temporary_file_decorator(self):
+        head, tails = os.path.split(self.test_temporary_file_decorator.filename)
+        self.assertEqual(settings.TEST_OUTPUT_PATH, head)
+        self.assertRegexpMatches(tails, r'^test_temporary_file_decorator_\d{8}_\d{4}\.json')
+
+        my_list = ['1', 'hola', 'poli', 'kilo']
+        with open(self.test_temporary_file_decorator.filename, 'w', encoding='utf-8') as mfile:
+            mfile.writelines(my_list)
+        self.assertTrue(os.path.exists(self.test_temporary_file_decorator.filename))
+
+    def test_temporary_file_decorator_no_delete(self):
+        @temporary_file('xlsx', delete_on_exit=False)
+        def my_function2():
+            my_list = ['1', 'hola', 'poli', 'kilo']
+            with open(my_function2.filename, 'w', encoding='utf-8') as mfile:
+                mfile.writelines(my_list)
+            return my_function2.filename
+        
+        filename = my_function2()
+        self.assertTrue(os.path.exists(filename))
+        os.remove(filename)
