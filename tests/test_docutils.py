77763e899449dcf8a2751b23d53290bd12c5def7
LBerrocal <LBerrocal@pancanal.com>
2017-06-10 08:22:00 -0500

    Creating and deleting basic doc structure

--- tests/test_docutils.py
+++ tests/test_docutils.py
@@ -1,5 +1,6 @@
 import os
 
+import shutil
 from django.test import TestCase
 
 from django_test_tools.doc_utils.folder_structure import create_folder_structure
@@ -8,9 +9,15 @@ from django_test_tools.utils import create_output_filename_with_date
 
 class TestFolderStructure(TestCase):
 
-    def test_(self):
+    def test_create_folder_structure(self):
         folder = create_output_filename_with_date('docs')
         if not os.path.exists(folder):
             os.makedirs(folder)
-        p = create_folder_structure(folder, 'example')
+        create_folder_structure(folder, 'django')
+        django_apps = ['admin', 'auth', 'contenttypes']
+        for djanoo_app in django_apps:
+            django_folder = os.path.join(folder, 'django', djanoo_app)
+            self.assertTrue(os.path.exists(django_folder))
+        shutil.rmtree(folder)
+
 
