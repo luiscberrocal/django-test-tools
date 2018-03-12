e2568bc171b3dcba4202df30616290696bea6ef1
Unknown <luis.berrocal.1942@gmail.com>
2018-03-12 16:55:15 -0500

    The name of of the test app was set in a settings variable.

--- tests/test_app_manager.py
+++ tests/test_app_manager.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.test import TestCase
 
 from django_test_tools.app_manager import DjangoAppManager
@@ -10,12 +11,13 @@ class TestDjangoAppManager(TestCase):
 
     def test_get_app(self):
         app_manager = DjangoAppManager()
-        app = app_manager.get_app('example.servers')
-        self.assertEqual('example.servers', app.name)
+        app = app_manager.get_app(settings.TEST_APP)
+        self.assertEqual(settings.TEST_APP, app.name)
 
     def test_get_project_apps(self):
         app_manager = DjangoAppManager()
-        apps = app_manager.get_project_apps('example')
+        app_module = settings.TEST_APP.split('.')[0]
+        apps = app_manager.get_project_apps(app_module)
         self.assertEqual(1, len(apps))
         apps = app_manager.get_project_apps('django')
         self.assertEqual(6, len(apps))
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -15,7 +15,7 @@ from django_test_tools.utils import create_output_filename_with_date
 
 class TestGenerateFactories(TestOutputMixin, TestCommandMixin, TestCase):
     def test_generate_factories(self):
-        call_command('generate_factories', 'example.servers', stdout=self.content)
+        call_command('generate_factories', settings.TEST_APP, stdout=self.content)
         results = self.get_results()
         self.assertEqual(44, len(results))
         filename = create_output_filename_with_date('example_my_app_factory.py')
@@ -63,14 +63,14 @@ class TestModelFactoryGenerator(TestCase):
 
 class TestGenerateModelTestCasesCommand(TestOutputMixin, TestCommandMixin, TestCase):
     def test_generate(self):
-        call_command('generate_model_test_cases', 'example.servers', stdout=self.content)
+        call_command('generate_model_test_cases', settings.TEST_APP, stdout=self.content)
         results = self.get_results()
         self.assertEqual(106, len(results))
 
 
 class TestGenerateSerializersCommand(TestOutputMixin, TestCommandMixin, TestCase):
     def test_generate(self):
-        call_command('generate_serializers', 'example.servers', stdout=self.content)
+        call_command('generate_serializers',settings.TEST_APP, stdout=self.content)
         results = self.get_results()
         self.assertEqual(22, len(results))
 
