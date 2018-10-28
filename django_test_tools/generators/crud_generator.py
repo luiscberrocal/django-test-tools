8f07d75bbe43b7a39b4205a751a4787793b72043
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-10-28 07:57:14 -0500

    Changed to Jinja template to generate urls

--- django_test_tools/generators/crud_generator.py
+++ django_test_tools/generators/crud_generator.py
@@ -1,23 +1,15 @@
-
+from django.template.loader import render_to_string
 
 
 class UrlGenerator(object):
-    URL_CRUD_TEMPLATE = """# -*- coding: utf-8 -*-
-from django.conf.urls import url
-from django.views.generic import TemplateView
-from .views import {0}ListView, {0}CreateView, {0}UpdateView, {0}DeleteView, {0}DetailView
-
-urlpatterns = [
-    url(r'^$', {0}ListView.as_view(), name='list'),
-    url(r'^create/$', {0}CreateView.as_view(), name='create'),
-    url(r'^update/(?P<pk>\d+)/$', {0}UpdateView.as_view(), name='update'),
-    url(r'^delete/(?P<pk>\d+)/$', {0}DeleteView.as_view(), name='delete'),
-    url(r'^detail/(?P<pk>\d+)/$', {0}DetailView.as_view(), name='detail'),
-]"""
 
     def __init__(self, model_name):
         self.model_name = model_name
+        self.template = 'django_test_tools/urls.py.j2'
 
     def print_urls(self, filename):
+        data = dict()
+        data['model_name'] = self.model_name
+        rendered = render_to_string(self.template, data)
         with open(filename, 'w', encoding='utf-8') as url_file:
-            url_file.write(self.URL_CRUD_TEMPLATE.format(self.model_name))
+            url_file.write(rendered)
