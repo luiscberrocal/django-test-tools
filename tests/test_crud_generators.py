3dc3ddd7b1d1fa3d2203147c039483ec5fb99668
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2019-01-27 16:53:26 -0500

    Got tests serializer working with jinja2

--- tests/test_crud_generators.py
+++ tests/test_crud_generators.py
@@ -1,8 +1,12 @@
 import os
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 from django_test_tools.file_utils import hash_file, temporary_file
-from django_test_tools.generators.crud_generator import UrlGenerator
+from django_test_tools.generators.crud_generator import UrlGenerator, SerializerTestGenerator
+try:
+    from example.servers.api.serializers import ServerSerializer
+except:
+    from servers.api.serializers import ServerSerializer
 
 
 class TestUrlGenerator(TestCase):
@@ -24,3 +28,31 @@ class TestUrlGenerator(TestCase):
         self.assertTrue(os.path.exists(filename))
         hash = hash_file(filename)
         self.assertEqual(hash, 'de4251b6c78a5911b6c3440c580de05d4d417c59')
+
+
+class TestSerializerTestGenerator(SimpleTestCase):
+
+    @temporary_file('.py', delete_on_exit=False)
+    def test_print(self):
+        filename = self.test_print.filename
+        generator = SerializerTestGenerator()
+        data= dict()
+
+        serializer = ServerSerializer()
+        rep_ser = repr(serializer)
+        fields = list()
+        str_fields = list()
+        for field in serializer.fields:
+            fields.append(field)
+
+        for key, field in serializer.fields.fields.items():
+            if type(field).__name__ in ['CharField', 'TextField']:
+                str_fields.append(field.field_name)
+            print(key)
+
+        data['model_name'] = serializer.Meta.model.__name__
+        data['fields'] = fields
+        data['string_vars'] = str_fields
+        generator.print(data, filename)
+
+        self.assertEqual(1, 1)
