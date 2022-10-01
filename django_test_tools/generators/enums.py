b22d25a391ce3253c1636f16920d0aebdac29f15
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2022-10-01 13:15:45 -0500

    Enums

--- django_test_tools/generators/enums.py
+++ django_test_tools/generators/enums.py
@@ -1,16 +1,17 @@
 from enum import Enum
 
 
-class FieldType(str, Enum):
-    """Supported fields."""
-    BOOLEAN_FIELD = 'BooleanField'
-    CHAR_FIELD = 'CharField'
-    COUNTRY_FIELD = 'CountryField'
-    DATE_FIELD = 'DateField'
-    DATETIME_FIELD = 'DateTimeField'
-    DECIMAL_FIELD = 'DecimalField'
+class SupportedFields(str, Enum):
+    BOOLEAN = 'BooleanField'
+    CHAR = 'CharField'
+    COUNTRY = 'CountryField'
+    DATE = 'DateField'
+    DATETIME = 'DateTimeField'
+    DECIMAL = 'DecimalField'
     FOREIGNKEY = 'ForeignKey'
-    GENERIC_IP_ADDRESS_FIELD = 'GenericIPAddressField'
-    INTEGER_FIELD = 'IntegerField'
-    MONEY_FIELD = 'MoneyField'
-    TEXT_FIELD = 'TextField'
+    GENERIC_IP_ADDRESS = 'GenericIPAddressField'
+    INTEGER = 'IntegerField'
+    MONEY = 'MoneyField'
+    TEXT = 'TextField'
+
+
