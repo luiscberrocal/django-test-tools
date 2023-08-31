23078f5cf19ac110789a2e70e5b8b2a92b2b83dd
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-31 08:12:31 -0500

    Tests for handlers

--- django_test_tools/generators/models.py
+++ django_test_tools/generators/models.py
@@ -1,16 +1,8 @@
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from pydantic import BaseModel, Field
 
 from django_test_tools.generators.enums import FieldType
-
-
-class FieldAttributes(BaseModel):
-    max_length: Optional[int] = Field(description='Maximum length of field. Used by CharField.')
-    max_digits: Optional[int] = Field(description='Maximum number of digits. Used by DecimalField')
-    decimal_places: Optional[int] = Field(description='Decimal places. Used by DecimalField')
-    positive: Optional[bool] = Field(description='Are values only positive? Used by DecimalField')
-    model_name: Optional[str] = Field(description='Name of the foreign model. Used by ForeignKey')
 
 
 class FieldInfo(BaseModel):
@@ -19,7 +11,6 @@ class FieldInfo(BaseModel):
     primary_key: bool
     editable: bool
     factory_entry = Optional[str]
-    # attributes: Optional[FieldAttributes]
     required_imports: Optional[List[str]]
     max_length: Optional[int] = Field(description='Maximum length of field. Used by CharField.')
     choice_type: Optional[str]
@@ -35,3 +26,8 @@ class FieldInfo(BaseModel):
 class ModelInfo(BaseModel):
     model_name: str
     fields: List[FieldInfo]
+
+
+class AppInfo(BaseModel):
+    app_name: str
+    models: Dict[str, Any]
--- tests/generators/handlers/test_factory_boy_handlers.py
+++ tests/generators/handlers/test_factory_boy_handlers.py
@@ -1,7 +1,55 @@
+from unittest import mock
 
 from django.test import SimpleTestCase
+
+from django_test_tools.generators.enums import FieldType
+from django_test_tools.generators.handlers.factory_boy_handlers import DateTimeFieldHandler
+from django_test_tools.generators.models import FieldInfo
+
+
+# Mocking FieldInfo class as it seems to be an external dependency not provided.
+class MockFieldInfo:
+    def __init__(self, type, field_name):
+        self.type = type
+        self.field_name = field_name
+        self.required_imports = []
+        self.factory_entry = None
+
+
+# Mocking FieldType enum as it seems to be an external dependency not provided.
+class MockFieldType:
+    DATETIME = "DATETIME"
+    FOREIGN_KEY = "FOREIGN_KEY"
+    INTEGER = "INTEGER"
+    TEXT = "TEXT"
+
+
+# Mocked settings.TIME_ZONE for the DateTimeFieldHandler
+class MockSettings:
+    TIME_ZONE = "UTC"
+
+
+# Test for DateTimeFieldHandler
 class TestDateTimeFieldHandler(SimpleTestCase):
 
-    def test_x(self):
-        pass
+    def setUp(self):
+        self.handler = DateTimeFieldHandler()
 
+    def test_handle_datetime_field(self):
+        field_info = FieldInfo(type=FieldType.DATETIME, field_name="created_at")
+
+        with mock.patch("django.conf.settings", MockSettings):
+            result = self.handler.handle(field_info)
+
+        self.assertEqual(result.required_imports, [
+            'from django.conf import settings',
+            'from factory import LazyAttribute',
+            'from faker import Factory as FakerFactory',
+            'faker = FakerFactory.create()'
+        ])
+        self.assertIn("faker.date_time_between", result.factory_entry)
+
+    def test_handle_non_datetime_field(self):
+        field_info = MockFieldInfo(MockFieldType.TEXT, "description")
+        result = self.handler.handle(field_info)
+        self.assertIsNone(result)
