e99fca62e39124eddf27e606fa06fa72f6794b1c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-08-31 13:45:20 -0500

    Expanding test coverage.

--- tests/generators/handlers/test_factory_boy_handlers.py
+++ tests/generators/handlers/test_factory_boy_handlers.py
@@ -4,7 +4,7 @@ from django.test import SimpleTestCase
 
 from django_test_tools.generators.enums import FieldType
 from django_test_tools.generators.handlers.factory_boy_handlers import DateTimeFieldHandler, TextFieldHandler, \
-    IntegerFieldHandler
+    IntegerFieldHandler, DateFieldHandler, CharFieldIdHandler
 from django_test_tools.generators.models import FieldInfo
 
 
@@ -78,6 +78,62 @@ class TestTextFieldHandler(SimpleTestCase):
         self.assertIn("faker.paragraph", result.factory_entry)
 
     def test_handle_non_text_field(self):
+        field_info = FieldInfo(type=FieldType.INTEGER, field_name="age")
+        result = self.handler.handle(field_info)
+        self.assertIsNone(result)
+
+
+class TestDateFieldHandler(SimpleTestCase):
+
+    def setUp(self):
+        self.handler = DateFieldHandler()
+
+    def test_handle_date_field(self):
+        field_info = FieldInfo(type=FieldType.DATE, field_name="paid_on")
+
+        with mock.patch("django.conf.settings", MockSettings):
+            result = self.handler.handle(field_info)
+
+        self.assertEqual(result.required_imports, [
+            'from django.conf import settings',
+            'from factory import LazyAttribute',
+            'from faker import Factory as FakerFactory',
+            'faker = FakerFactory.create()'
+        ])
+        self.assertIn("faker.date_between", result.factory_entry)
+
+    def test_handle_non_date_field(self):
+        field_info = FieldInfo(type=FieldType.TEXT, field_name="description")
+        result = self.handler.handle(field_info)
+        self.assertIsNone(result)
+
+
+class TestCharFieldIdHandler(SimpleTestCase):
+
+    def setUp(self):
+        self.handler = CharFieldIdHandler()
+
+    def test_handle_char_id_field(self):
+        field_info = FieldInfo(type=FieldType.CHAR, field_name="device_id", max_length=32)
+        result = self.handler.handle(field_info)
+
+        self.assertEqual(result.required_imports, ['import string', 'from factory import LazyAttribute',
+                                                   'from factory.fuzzy import FuzzyText'])
+        expected = (f'LazyAttribute(lambda x: FuzzyText(length={field_info.max_length}, '
+                    f'chars=string.digits).fuzz())')
+        self.assertEqual(result.factory_entry, expected)
+
+    def test_handle_char_id_ascii(self):
+        field_info = FieldInfo(type=FieldType.CHAR, field_name="device", max_length=16)
+        result = self.handler.handle(field_info)
+
+        self.assertEqual(result.required_imports, ['import string', 'from factory import LazyAttribute',
+                                                   'from factory.fuzzy import FuzzyText'])
+        expected = (f'LazyAttribute(lambda x: FuzzyText(length={field_info.max_length}, '
+                    f'chars=string.ascii_letters).fuzz())')
+        self.assertEqual(result.factory_entry, expected)
+
+    def test_handle_non_char_id_field(self):
         field_info = FieldInfo(type=FieldType.INTEGER, field_name="age")
         result = self.handler.handle(field_info)
         self.assertIsNone(result)
